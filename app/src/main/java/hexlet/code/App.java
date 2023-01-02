7ceec3650b4689105880f9352fa180d64e8cc862
Shaiko-Vitaliy <shaiko2586@gmail.com>
2023-01-02 16:56:50 +0300

    add arguments and optionals

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,13 +5,21 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-f", "--format"}, paramLabel = "format",description = "output format [default: stylish]")
+    int format;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1 ;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
